86ad53cd08eb4714b3b370b21ae111332eed1fa9
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-07-19 16:44:01 -0400

    Update create_request.js

--- create_request.js
+++ create_request.js
@@ -52,7 +52,7 @@ function createRequest(e){
 		response = "A Number";
 		should_stop = true;
 	}
-	else if (!phone_number || !phone_number.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;)))
+	else if (!phone_number || !phone_number.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/))
 	{
 		response = "Phone Number";
 		should_stop = true;
