4bbd47d8e55055e531622ee3df379a8dfc529363
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-07-23 10:21:24 -0400

    Update create_request.js

--- create_request.js
+++ create_request.js
@@ -156,6 +156,7 @@ function createRequest(){
             url: Url,
             data: requestData,
             dataType: "text",
+            async: false
             success: function(result){
             },
             error: function(result){
