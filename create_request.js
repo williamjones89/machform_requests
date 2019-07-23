6aa0c4fe3172a0e931f20ac53757e854a5e252ac
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-07-23 08:47:47 -0400

    Update create_request.js

--- create_request.js
+++ create_request.js
@@ -6,7 +6,9 @@ $(document).ready(
     function()
     {
         document.forms[0].addEventListener("submit", createRequest);
-        document.getElementById("form_416097").action = '';
+        document.forms[0].addEventListener("submit", function(event) {
+            event.preventDefault();
+        }, false););
     }
 );
 
@@ -157,10 +159,8 @@ function createRequest(e){
             url: Url,
             data: requestData,
             success: function(result){
-                console.log(result);
             },
             error: function(result){
-                console.log(result);
             },
             complete: function(){
                 console.log('Request Submission Finished');
@@ -175,32 +175,31 @@ function createRequest(e){
             url: "/machform/view.php",
             data: $('form').serialize(),
             success: function(result){
-                console.log(result);
             },
             error: function(result){
-                console.log(result);
             },
             complete: function(){
                 console.log('Form Submission Finished');
-
-                // Hide Form
-                var form_to_hide = document.forms[0];
-                if (form_to_hide.style.display === "none") {
-                    form_to_hide.style.display = "block";
-                } else {
-                    form_to_hide.style.display = "none";
-                }
-                
-                // Create a new DIV with text in it and insert it where the form was
-                var div_element = document.createElement('div');
-                var para = document.createElement("H2");
-                var t = document.createTextNode("Success! Your submission has been saved!");
-                para.appendChild(t);
-                div_element.style.textAlign = "center";
-                div_element.setAttribute('class', 'form_success');
-                div_element.appendChild(para);
-                document.getElementById("form_container").insertBefore(div_element, form_to_hide);
             }
         }
     );
+    
+     // Hide Form
+    var form_to_hide = document.forms[0];
+    if (form_to_hide.style.display === "none") {
+        form_to_hide.style.display = "block";
+    } 
+    else {
+        form_to_hide.style.display = "none";
+    }
+
+    // Create a new DIV with text in it and insert it where the form was
+    var div_element = document.createElement('div');
+    var para = document.createElement("H2");
+    var t = document.createTextNode("Success! Your submission has been saved!");
+    para.appendChild(t);
+    div_element.style.textAlign = "center";
+    div_element.setAttribute('class', 'form_success');
+    div_element.appendChild(para);
+    document.getElementById("form_container").insertBefore(div_element, form_to_hide);
 };
