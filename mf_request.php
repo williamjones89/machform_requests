1c8166048a6fa52ff6bfd14a97192f0b3031ad59
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-07-25 06:54:37 -0400

    Update mf_request.php

--- mf_request.php
+++ mf_request.php
@@ -1,12 +1,12 @@
 <?php
-
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://webhook.site/699a57c5-679e-42ba-a94d-ca0595ec55c6");
+curl_setopt($ch, CURLOPT_URL, "https://servicedesk.sunyorange.edu:443/api/v3/requests?TECHNICIAN_KEY=");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_TIMEOUT, 100);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
 curl_exec($ch);
 curl_close($ch);
 
 echo 'success';
+exit;
 ?>
