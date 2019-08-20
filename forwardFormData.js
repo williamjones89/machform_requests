0854062e0228d41b481f21c1d5a1ef6f16b3808e
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-08-20 10:17:40 -0400

    Update forwardFormData.js

--- forwardFormData.js
+++ forwardFormData.js
@@ -15,15 +15,15 @@ function createRequest(){
     $.ajax(
         {
             type: 'POST',
-            url: "/machform/view.php",
-            data: $('form').serialize(),
+            url: "/machform/so_php/get_form_values.php",
+            data: $('form').serialize() + "prev_form_id=417527&prev_entry_id=4",
             success: function(result){
             },
             error: function(result){
             },
             complete: function(){
                 console.log('Form Submission Finished');
-                console.log($('form').serialize());
+                console.log($('form').serialize() + "prev_form_id=417527&prev_entry_id=4");
             }
         }
     );
