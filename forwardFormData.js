6a8dcbdc734df15d4d82639521b17d1971d75ac6
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-08-20 15:39:51 -0400

    Update forwardFormData.js

--- forwardFormData.js
+++ forwardFormData.js
@@ -6,14 +6,14 @@ $(document).ready(
         var prevFormID = urlParams.get('prev_form_id');
         var prevEntryID = urlParams.get('prev_entry_id');
         
-        console.log("&prev_form_id=" + {prevFormID} + "&prev_entry_id=" + {prevEntryID})
+        console.log("&prev_form_id=" + prevFormID + "&prev_entry_id=" + prevEntryID)
         
         // &prev_form_id=417527&prev_entry_id=4
         $.ajax(
         {
             type: 'POST',
             url: "/machform/so_php/get_form_values.php",
-            data: "&prev_form_id=" + {prevFormID} + "&prev_entry_id=" + {prevEntryID},
+            data: "&prev_form_id=" + prevFormID + "&prev_entry_id=" + prevEntryID,
             success: function(result){
             },
             error: function(result){
