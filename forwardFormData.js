ee832560d3a4e7a4bf620fd19efe7f7bb52197f6
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-08-21 11:28:56 -0400

    Update forwardFormData.js

--- forwardFormData.js
+++ forwardFormData.js
@@ -25,8 +25,6 @@ $(document).ready(
                     
                     var jsonData = result.responseText;
                     var obj = JSON.parse(jsonData);
-                    
-                    console.log(obj.count);
                 }
             })
         }
