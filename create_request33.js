9f611077e531c18aea469f16e7e09b7eddd64777
williamjones89 <53062749+williamjones89@users.noreply.github.com>
2019-09-27 10:17:17 -0400

    Update create_request33.js

--- create_request33.js
+++ create_request33.js
@@ -6,10 +6,10 @@ $(document).ready(
     function()
     {
         var urlParams = new URLSearchParams(window.location.search);
-        console.log(urlParams.has('done'));
+        console.log("Submitted: " + urlParams.has('done'));
         if (urlParams.has('prev_entry'))
         {
-            console.log(urlParams.get('prev_entry'));
+            console.log("Entry id: " + urlParams.get('prev_entry'));
             // POST original form data
             $.ajax(
             {
@@ -26,7 +26,117 @@ $(document).ready(
                     var jsonData = result.responseText;
                     var obj = JSON.parse(jsonData);
                     
-                    console.log(obj);
+                     // Hide Form
+                    var form_to_hide = document.forms[0];
+                    if (form_to_hide.style.display === "none") {
+                        form_to_hide.style.display = "block";
+                    } 
+                    else {
+                        form_to_hide.style.display = "none";
+                    }
+                    
+                    var student_role = obj["element_1"];
+                    var first_name = obj["element_2"];
+                    var last_name = obj["element_3"];
+                    var a_number = obj["element_4"];
+                    var phone_number = obj["element_9_1"] + "-" + obj["element_9_2"] + "-" + obj["element_9_3"];
+                    var alt_email = obj["element_6"];
+                    var last_login = obj["element_7"];
+                    var description = obj["element_8"];
+                    
+                     // Create JSON to send to API URL using variables we created.
+                    var myJson = {
+                        "request": {
+                            "requester": {
+                                "email_id": "",
+                                "name": "Default User",
+                                "is_vipuser": "false",
+                                "id": "13203"
+                            },
+                            "subject": "Web Form Login Issue",
+                            "request_type": {
+                                "name": "Incident/Interruption of Service",
+                                "id": "902"
+                            },
+                            "category": {
+                                "name": "INCIDENTS OR INTERRUPTION OF SERVICE",
+                                "id": "602"
+                            },
+                            "subcategory": {
+                                "name": "Login/Account Issue/Failure",
+                                "id": "1220"
+                            },
+                            "item": {
+                                "name": "Login/Password Error",
+                                "id": "3399"
+                            },
+                            "group": {
+                                "name": "Service Desk (ITSD)",
+                                "id": "301"
+                            },
+                            "description": `<p>${description}</p><br><p>Student role: ${student_role}</p><br><p>Last login: ${last_login}</p><br><p>${first_name} ${last_name}</p><p>${phone_number}</p><p>${alt_email}</p><p>${a_number}</p>`,
+                            "mode": {
+                                "name": "4-Web Case",
+                                "id": "2"
+                            },
+                            "impact": {
+                                "name": "4-Minor/Localized",
+                                "id": "3"
+                            },
+                            "priority": {
+                                "color": "#939393",
+                                "name": "4-Low",
+                                "id": "1"
+                            },
+                            "urgency": {
+                                "name": "4-Low",
+                                "id": "4"
+                            },
+                            "udf_fields": {
+                                "udf_multiselect_3610": [
+                                    "MySUNYOrange"
+                                ],
+                                "udf_sline_2705": "N/A",
+                                "udf_sline_2703": `${phone_number}`,
+                                "udf_pick_2702": "OFF-Campus Site"
+                            }
+                        }
+                    }
+
+                    // Convert JSON to string
+                    var jsonString = JSON.stringify(myJson);
+
+                    // Add "input_data=" to the beginning of the jsonString.
+                    // Encode non-letter characters (Ex: } becomes %7D)
+                    var requestData = 'input_data=' + encodeURIComponent(jsonString);
+
+                    console.log("Submitting Request");
+
+                    // POST requestData to service_desk_request
+                    $.ajax(
+                        {
+                            type: 'POST',
+                            url: Url,
+                            data: requestData,
+                            success: function(result){
+                            },
+                            error: function(result){
+                            },
+                            complete: function(){
+                                console.log('Request Submission Finished');
+                            }
+                        }
+                    );
+
+                    // Create a new DIV with text in it and insert it where the form was
+                    var div_element = document.createElement('div');
+                    var para = document.createElement("H2");
+                    var t = document.createTextNode("Success! Your submission has been saved!");
+                    para.appendChild(t);
+                    div_element.style.textAlign = "center";
+                    div_element.setAttribute('class', 'form_success');
+                    div_element.appendChild(para);
+                    document.getElementById("form_container").insertBefore(div_element, form_to_hide);
                 }
             })
         }
